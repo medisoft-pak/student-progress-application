c868c3f210f35fdac01ac6b66a4730fa026be462
inzamam <malikasinger@gmail>
2015-10-22 06:44:57 -0700

    fileopner

--- student progress system/www/js/app.js
+++ student progress system/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic','ngCordova'])
 
 /*
 .run(function($ionicPlatform) {
@@ -45,12 +45,40 @@ angular.module('starter', ['ionic'])
       })
       .state('schoolSetup', {
         url: '/schoolSetup',
-        templateUrl: 'views/schoolSetup/schoolSetup.html'
+        templateUrl: 'views/schoolSetup/schoolSetup.html',
+        controller : "FileOpenerController"
+      })
+      .state('logIn', {
+        url: '/logIn',
+        templateUrl: 'views/logIn/logIn.html'
         //controller : "CtrlChngeValue"
       })
+      .state('dashboard', {
+        url: '/dashboard',
+        templateUrl: 'views/dashboard/dashboard.html'
+        //controller : "CtrlChngeValue"
+      })
+    $urlRouterProvider.otherwise("/dashboard");
+
+  })
+
+.controller('FileOpenerController', function($scope, $cordovaFileOpener2, $ionicPlatform) {
 
 
-    $urlRouterProvider.otherwise("/schoolSetup");
+  $scope.openfiles = function () {
+    $cordovaFileOpener2.open(
+    /*  '/sdcard/Download/starwars.pdf',
+      'application/pdf'*/
+       '/Phone storage/Download/Index.pdf',
+       'application/pdf'
 
-  });
+    ).then(function () {
+        // file opened successfully, $ionicPlatform
+        alert("sfsfsfsss");
 
+      }, function (err) {
+        //console.log('Error status: ' + err.status + ' - Error message: ' + err.message);
+        console.log('An error occurred: ' + JSON.stringify(err));
+      });
+  }
+});
