80369459dcb00c6253d93a5ed0e55d9842811c63
inzamam <malikasinger@gmail>
2015-10-26 03:10:27 -0700

    syntax error corrected

--- student progress system/www/views/studentRegistration/studentRegistration.js
+++ student progress system/www/views/studentRegistration/studentRegistration.js
@@ -1,3 +1,28 @@
 /**
  * Created by kamran on 10/20/2015.
  */
+/*angular.module('starter')
+
+/// Scound Control
+  .controller('CtrlplaybackScound', function($scope) {
+    ion.sound({
+      sounds: [
+        {
+          name: "tic-tac-toe-theme",
+          loop: true
+        }
+
+      ],
+
+      path: "./sounds/",
+      preload: true,
+      multiplay: true,
+      volume: 1
+    });
+    ion.sound.play("tic-tac-toe-theme");
+    $scope.playbacksound2 = function () {
+      ion.sound.play("tic-tac-toe-theme");
+    }
+
+
+  })*/
