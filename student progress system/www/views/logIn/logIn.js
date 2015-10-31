35bd05b024567e9ae6dacc3f4b439f7004035311
inzamam malik <malikasinger@gmail.com>
2015-10-31 00:24:40 -0700

    login page created and set as default

--- student progress system/www/views/logIn/logIn.js
+++ student progress system/www/views/logIn/logIn.js
@@ -1,3 +1,11 @@
 /**
  * Created by kamran on 10/20/2015.
  */
+angular.module('starter')
+
+  .controller('logInController', function ($scope, dataService) {
+
+    $scope.loginData = {};
+
+
+  });
